25f336fe5380fd94378acb1a2644dd7acc8214cd
J-LopezE <jorgeisrael.lopez@gmail.com>
2026-04-23 14:54:10 -0600

    feat: add database setup with SQLAlchemy, Flask-Migrate and User model

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from flask_restx import Api
-from app.routes.health import ns
+from flask_migrate import Migrate
+from app.config import Config
+from app.models import db
+
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    Migrate(app, db)
+
+    with app.app_context():
+        from app import models 
+
+    from app.routes.health import ns
     api = Api(app, prefix="/api", title="Backend Challenge", version="1.0", description="REST API")
     api.add_namespace(ns)
     return app
