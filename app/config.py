403fa8c69da0bad704245d577067ced3d57a4c2f
J-LopezE <jorgeisrael.lopez@gmail.com>
2026-04-28 14:39:18 -0600

    feat: add environment-based configuration for development, testing and production

--- app/config.py
+++ app/config.py
@@ -4,3 +4,21 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    
+config = {
+     "development": DevelopmentConfig,
+    "testing":     TestingConfig,
+    "production":  ProductionConfig,
+    "default":     DevelopmentConfig,
+}
